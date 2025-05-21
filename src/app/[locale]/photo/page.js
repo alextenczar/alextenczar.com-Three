7407a9e5191823a638403d2cf9204612d1c9797b
Alex Tenczar <24301038+alextenczar@users.noreply.github.com>
2025-05-21 17:48:17 +0900

    Testing revert of image page with different env var url

--- src/app/[locale]/photo/page.js
+++ src/app/[locale]/photo/page.js
@@ -3,6 +3,16 @@ import { getTranslations } from "next-intl/server";
 
 export const dynamic = 'force-dynamic';
 
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 export async function generateMetadata({ params }) {
   const { locale } = params;
   const t = await getTranslations({ locale, namespace: 'Metadata' });
@@ -14,12 +24,18 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function Photo() {
+  async function fetchPhotos() {
+    const res = await fetch(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/photos?quantity=100`, { cache: 'no-store' });
+    let response = await res.json();
+    return response.response;
+  }
+
+  const response = await fetchPhotos();
+  let images = shuffleArray(response.photos.photo);
 
   return (
     <main>
-      <div className='min-h-[100vh]'>
-        <ImageGrid />
-      </div>
+      <ImageGrid images={images} />
     </main>
   );
 }
